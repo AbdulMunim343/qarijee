69ccd00faf0fe1427d748b2a729533a9e4144691
Nameer waqas <nameerwaqas321@gmail.com>
2020-11-17 18:47:46 +0500

    Style: Add banner section in the homepage. Details: Added banner section to homepage and use react-reveal package to get animated effects with addition of a temporary background image. (#1)
    * Style: Add banner section in the homepage.
    Details: Added banner section to homepage and use react-reveal package to get animated effects
    with addition of a temporary background image.
    
    * Style: Made landing page little responsive
    
    * Style: Fixed the navigation bar at top.
    
    Co-authored-by: --global <--global>

--- client/src/containers/landinPage.js
+++ client/src/containers/landinPage.js
@@ -1,6 +1,10 @@
-import NavBar from '../components/navbar'
+import NavBar from '../components/navbar';
+import Banner from '../components/banner';
+import Courses from '../components/courses';
 export default function LandingPage(){
 return(<>
     <NavBar />
+    <Banner/>
+    <Courses/>
 </>)
 }
